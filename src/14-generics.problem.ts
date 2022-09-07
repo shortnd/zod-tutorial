1316a00e858f57082484c91f472f866b7efacbe4
shortnd <ococncol@gmail.com>
2022-09-07 11:21:41 -0400

    14 done

--- src/14-generics.problem.ts
+++ src/14-generics.problem.ts
@@ -4,7 +4,7 @@ import { it } from "vitest";
 import { z } from "zod";
 import { Equal, Expect } from "./helpers/type-utils";
 
-const genericFetch = (url: string, schema: z.ZodSchema) => {
+const genericFetch = <T>(url: string, schema: z.ZodSchema<T>) => {
   //                 ^ 🕵️‍♂️
   return fetch(url)
     .then((res) => res.json())
